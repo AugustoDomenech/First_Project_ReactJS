70c3640b633cc045599cc0f93a874ac80fefa6aa
Augusto Domenech <augustodomenech@gmail.com>
2019-11-10 10:42:43 -0300

    Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
- import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title error={false}>Main</Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form>
+        <input
+          type='text'
+          placeholder= 'Adicionar repositório'
+        />
+
+        <SubmitButton>
+          <FaPlus color='#fff' size={14}  />
+        </SubmitButton>
+
+      </Form>
+
+    </Container>
   );
 }
